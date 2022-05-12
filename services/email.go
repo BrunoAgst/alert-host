ad12e26d6151013adc473eb93b8fd2fcc85fa410
BrunoAgst <bruno_agst@hotmail.com>
2022-05-12 00:20:00 -0300

    fix name variables

--- services/email.go
+++ services/email.go
@@ -10,13 +10,13 @@ func sendNotification(message string) {
 	from := os.Getenv("FROM")
 	password := os.Getenv("PASSWORD")
 	to := os.Getenv("TO")
-	smtHost := os.Getenv("SMTPHOST")
-	smtPort := os.Getenv("SMTPPORT")
+	smtpHost := os.Getenv("SMTPHOST")
+	smtpPort := os.Getenv("SMTPPORT")
 	content := []byte("From: " + from + "\r\n" + "To: " + to + "\r\n" + "Subject: Alert Host Down!!!\r\n\r\n" + message + "\r\n")
 
-	auth := smtp.PlainAuth("", from, password, smtHost)
+	auth := smtp.PlainAuth("", from, password, smtpHost)
 
-	err := smtp.SendMail(smtHost+":"+smtPort, auth, from, []string{to}, content)
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, content)
 
 	if err != nil {
 		log.Fatal(err)
