8ca23dac20fe0e9d5cef8887e4e8c6401596b585
Bruno Augusto <bruno_agst@hotmail.com>
2022-11-01 17:17:17 -0300

    remove space

--- services/hosts.go
+++ services/hosts.go
@@ -9,7 +9,6 @@ func MonitorHosts(hosts []models.Host) {
 
 	for _, host := range hosts {
 		status, message := verifyHost(host.URL)
-
 		if !status {
 			sendNotification(message)
 		}
@@ -19,7 +18,7 @@ func MonitorHosts(hosts []models.Host) {
 func verifyHost(host string) (bool, string) {
 	res, _ := http.Get(host)
 
-	if res.StatusCode == 200 {
+	if res.StatusCode == 200 {		
 		return true, "host: " + host + " page ok"
 	}
 	return false, "host: " + host + " page down"
